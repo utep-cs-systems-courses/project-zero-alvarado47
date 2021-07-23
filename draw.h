e890f494b12bd7027b50fca6b5bcc05f5b76433c
alvarado47 <85764066+alvarado47@users.noreply.github.com>
2021-07-23 08:48:10 -0600

    Update draw.h

--- draw.h
+++ draw.h
@@ -3,7 +3,7 @@
 
 void print_triangle(int startCol, int size);
 void print_square(int startCol, int size);
-void print_char_11x16(char c);
+//void print_char_11x16(char c);
 void print_char_8x12(char c);
 void print_arrow();
 
