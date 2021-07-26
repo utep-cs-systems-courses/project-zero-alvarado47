e2ffba9b85e84635814065128e1435f2a7b63e48
alvarado47 <85764066+alvarado47@users.noreply.github.com>
2021-07-26 06:50:08 -0600

    Update uimain.c

--- uimain.c
+++ uimain.c
@@ -28,7 +28,7 @@ int main()
       puts("You selected chars:");
       for (char c = 'A'; c < 'D'; c++)
       //print_char_11x16(c);
-      print_char_8x12(c);
+      print_char_5x7(c);
       break;
     case 'a';
 	puts("You selected arrow:");
